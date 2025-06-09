350bde780aa8dded13739c4cdd9457479a0d04d1
bruce.lu <bruce.lu@bridgewell.com>
2025-06-09 06:32:22 +0000

    refactor: Add commented alternative implementation for productExceptSelf

--- leetcode/0238-product-of-array-except-self/solution.py
+++ leetcode/0238-product-of-array-except-self/solution.py
@@ -14,6 +14,21 @@ def productExceptSelf(nums: List[int]) -> List[int]:
         right *= nums[i]
     return answer
 
+# --- 補充：分別計算左右乘積再相乘的寫法 ---
+# def productExceptSelf(nums: List[int]) -> List[int]:
+#     n = len(nums)
+#     left_products = [1] * n
+#     right_products = [1] * n
+#     # 計算左乘積
+#     for i in range(1, n):
+#         left_products[i] = left_products[i-1] * nums[i-1]
+#     # 計算右乘積
+#     for i in range(n-2, -1, -1):
+#         right_products[i] = right_products[i+1] * nums[i+1]
+#     # 左右乘積相乘
+#     answer = [left_products[i] * right_products[i] for i in range(n)]
+#     return answer
+
 
 if __name__ == "__main__":
     # 測試範例
